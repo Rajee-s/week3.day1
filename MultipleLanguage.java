b2ed9da30de1dc5b91591f4787dfdeeb58dae221
Rajee-s <108996671+Rajee-s@users.noreply.github.com>
2022-07-24 01:04:00 +0530

    Add files via upload

---
+++ MultipleLanguage.java
@@ -0,0 +1,24 @@
+package week3.day1;
+
+public class MultipleLanguage implements TestTool {
+	
+	public void python() {
+		
+	}
+	
+	public void ruby() {
+		
+	}
+
+	public void Selenium() {
+		
+		
+	}
+
+	public void Java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
