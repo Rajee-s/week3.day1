9c33af0cf9bee3924d8f9718a7f9103979bc6284
Rajee-s <108996671+Rajee-s@users.noreply.github.com>
2022-07-25 15:13:43 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,32 @@
+package week3.day1;
+
+public class Automation extends MultipleLanguage {
+
+
+	public void Selenium() {
+		System.out.println("Welcome to Selenium");
+		
+	}
+
+	public void Java() {
+		System.out.println("Java Programming");
+		
+	}
+	
+
+	
+	public static void main(String[] args) {
+		Automation au = new Automation();
+		au.Java();
+		au.Selenium();
+		au.python();
+		au.ruby();
+	}
+
+	
+	
+	
+	
+	
+
+}
---
+++ MultipleLanguage.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public abstract class MultipleLanguage implements TestTool, Language {
+	
+	public void python() {
+		System.out.println("This is Python Programming");
+	}
+
+	public void ruby() {
+		
+	}
+		
+	
+}
